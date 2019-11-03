5d99869d7a8ee90423ff5015f8e0b2c5b006d80d
Christopher Konopka <cskonopka@gmail.com>
2019-11-03 02:44:52 -0500

    added comments to usgs

--- usgs/usgs.js
+++ usgs/usgs.js
@@ -13,25 +13,31 @@ function anypost(str) {
 
 let str = "";
 Max.addHandler("usgs", (...string) => {
+    // start = starting date range
     let start = string[0];
+    // end = end date range
     let end = string[1];
+    // USGS GET Request
     let url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=" + start + "&endtime=" + end;
-
-    // let url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-10-18&endtime=2019-10-19";
 
     https.get(url, res => {
         res.setEncoding("utf8");
         let body = "";
+        // receive data body
         res.on("data", data => {
             body += data;
         });
         res.on("end", () => {
+            // parse the JSON
             body = JSON.parse(body);
+            // access the "features" part of the JSON body
             for (i = 0; i < body.features.length; i++) {
-                // magnitudes
+                // access the magnitudes from the properties array
                 anypost(body.features[i].properties.mag);
+                // concat the magnitudes into a string
                 str += body.features[i].properties.mag + " ";
             }
+            // output the magnitudes to Max
             Max.outlet(str);
             str = "";
         });
